19720fcd292b708799ac5f51ff53689c6a286405
shuyx <18271801652@163.com>
2025-12-02 17:16:03 +0800

    自动更新: 2025-12-02 17:16:03

---
+++ README.md
@@ -0,0 +1,11 @@
+# shuyx-wxminiprogram-ui
+
+个人微信小程序前台页面代码。
+
+使用微信官方IDE-微信开发者工具编写。
+
+技术栈
+- 使用colorui ui组件库
+
+
+
---
+++ app.js
@@ -0,0 +1,24 @@
+// app.js
+App({
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  globalData: {
+    //小程序版本号
+    version:"v1.01",
+    userInfo: null,
+    //用户的openId和unionId
+    openId:null,
+    token:null,
+  }
+})
